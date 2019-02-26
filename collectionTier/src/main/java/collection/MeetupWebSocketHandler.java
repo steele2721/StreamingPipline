068b2af0cbf692bf010f85f8c6a9ba4901bdf4af
robertsteele <steele.210@osu.edu>
2019-02-25 22:16:00 -0500

    Initial commit

---
+++ collectionTier/src/main/java/collection/MeetupWebSocketHandler.java
@@ -0,0 +1,15 @@
+package collection;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+@Component
+public class MeetupWebSocketHandler extends AbstractWebSocketHandler {
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) {
+        System.out.println(message.getPayload());
+    }
+}
